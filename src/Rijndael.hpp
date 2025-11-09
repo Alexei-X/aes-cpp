8f8b1febcb63b89cdf2e2bef8ce1c18532c68d81
Alex Bataille <alex.bataille@ulb.be>
2025-11-09 15:07:53 +0100

    Added ShiftRow step

--- src/Rijndael.hpp
+++ src/Rijndael.hpp
@@ -7,13 +7,34 @@ using State = std::array<std::array<uint8_t, 4>, 4>;
 
 class Rijndael {
 private:
-    int block_size;
+    int block_size, key_size;
     State state;
     SBox sbox;
+
+    void swap(std::array<uint8_t, 4> *arr_p, int i1, int i2) {
+        uint8_t temp = (*arr_p)[i1];
+        (*arr_p)[i1] = (*arr_p)[i2];
+        (*arr_p)[i2] = temp;
+    }
+
+    void shift(std::array<uint8_t, 4> arr, int shift_value) {
+        for (int it = 0; it < shift_value; it++) {
+            swap(&arr, 0, 1);
+            swap(&arr, 1, 2);
+            swap(&arr, 2, 3);
+            swap(&arr, 3, 4);
+        }
+    }
+
 public:
-    Rijndael(int block_size) : block_size(block_size) {
+    Rijndael(int block_size, int key_size) : block_size(block_size), key_size(key_size) {
+        if (block_size % 32 != 0 || key_size % 32 != 0) {
+            block_size = 128;
+            key_size = 128;
+        }
         this->sbox = SBox();
-    };
+    }
+
     void SubBytesStep() {
         for (int line = 0; line < 4; line++) {
             for (int column = 0; column < 4; column++) {
@@ -21,9 +42,16 @@ public:
             }
         }
     }
-    void ShiftRowsStep() {}
+
+    void ShiftRowsStep() {
+        for (int i = 1; i < 4; i++)
+            shift(this->state[i], i);
+    }
+
     void MixColumnsStep() {}
+
     void AddRoundKeyStep() {}
+
     void printState() {
         for (int line = 0; line < 4; line++) {
             for (int column = 0; column < 4; column++) {
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "Rijndael.hpp"
 
 int main() {
-    Rijndael rijn = Rijndael(192);
+    Rijndael rijn = Rijndael(128, 192);
     SBox sbox = SBox();
     std::cout << std::hex << (int) sbox.get_substitute(0x5A) << std::endl;
     return 0;
